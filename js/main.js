dbb013f7d9df8f05e5bd6343d2198167fa3ef4e4
Marieglahn <marieglahn@hotmail.com>
2016-03-11 10:26:10 +0100

    split js into functions

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+var defaultLayerString = 'OpenStreetMap.Mapnik';
 var map = L.map('map', {
     center: [48, -3],
     zoom: 5,
@@ -6,90 +7,40 @@ var map = L.map('map', {
 
 map.setView([51.51032167, -0.187084072], 13);
 
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
-
-var defaultLayer = L.tileLayer.provider('OpenStreetMap.Mapnik').addTo(map);
-
-var baseLayers = {
-    'OpenStreetMap Default': defaultLayer,
-    'OpenStreetMap German Style': L.tileLayer.provider('OpenStreetMap.DE'),
-    'OpenStreetMap Black and White': L.tileLayer.provider('OpenStreetMap.BlackAndWhite'),
-    'OpenStreetMap H.O.T.': L.tileLayer.provider('OpenStreetMap.HOT'),
-    'Thunderforest OpenCycleMap': L.tileLayer.provider('Thunderforest.OpenCycleMap'),
-    'Thunderforest Transport': L.tileLayer.provider('Thunderforest.Transport'),
-    'Thunderforest Landscape': L.tileLayer.provider('Thunderforest.Landscape'),
-    'Hydda Full': L.tileLayer.provider('Hydda.Full'),
-    'MapQuest OSM': L.tileLayer.provider('MapQuestOpen.OSM'),
-    'MapQuest Aerial': L.tileLayer.provider('MapQuestOpen.Aerial'),
-    'Stamen Toner': L.tileLayer.provider('Stamen.Toner'),
-    'Stamen Terrain': L.tileLayer.provider('Stamen.Terrain'),
-    'Stamen Watercolor': L.tileLayer.provider('Stamen.Watercolor'),
-    'Esri WorldStreetMap': L.tileLayer.provider('Esri.WorldStreetMap'),
-    'Esri DeLorme': L.tileLayer.provider('Esri.DeLorme'),
-    'Esri WorldTopoMap': L.tileLayer.provider('Esri.WorldTopoMap'),
-    'Esri WorldImagery': L.tileLayer.provider('Esri.WorldImagery'),
-    'Esri WorldTerrain': L.tileLayer.provider('Esri.WorldTerrain'),
-    'Esri WorldShadedRelief': L.tileLayer.provider('Esri.WorldShadedRelief'),
-    'Esri WorldPhysical': L.tileLayer.provider('Esri.WorldPhysical'),
-    'Esri OceanBasemap': L.tileLayer.provider('Esri.OceanBasemap'),
-    'Esri NatGeoWorldMap': L.tileLayer.provider('Esri.NatGeoWorldMap'),
-    'Esri WorldGrayCanvas': L.tileLayer.provider('Esri.WorldGrayCanvas')
-};
-
-var overlayLayers = {
-    //    'OpenSeaMap': L.tileLayer.provider('OpenSeaMap'),
-    //    'OpenWeatherMap Precipitation': L.tileLayer.provider('OpenWeatherMap.Precipitation'),
-    //    'OpenWeatherMap PrecipitationClassic': L.tileLayer.provider('OpenWeatherMap.PrecipitationClassic'),
-    //    'OpenWeatherMap Rain': L.tileLayer.provider('OpenWeatherMap.Rain'),
-    //    'OpenWeatherMap RainClassic': L.tileLayer.provider('OpenWeatherMap.RainClassic'),
-    //    'OpenWeatherMap Pressure': L.tileLayer.provider('OpenWeatherMap.Pressure'),
-    //    'OpenWeatherMap Temperature': L.tileLayer.provider('OpenWeatherMap.Temperature'),
-    //    'OpenWeatherMap Snow': L.tileLayer.provider('OpenWeatherMap.Snow')
-};
-
-var layerControl = L.control.layers(baseLayers, overlayLayers, {
-    collapsed: false
-}).addTo(map);
-
-// resize layers control to fit into view.
-function resizeLayerControl() {
-    var layerControlHeight = document.body.clientHeight - (10 + 50);
-    var layerControl = document.getElementsByClassName('leaflet-control-layers-expanded')[0];
-
-    layerControl.style.overflowY = 'auto';
-    layerControl.style.maxHeight = layerControlHeight + 'px';
-}
-map.on('resize', resizeLayerControl);
-resizeLayerControl();
+//L.marker([51.5, -0.09]).addTo(map)
+//    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+//    .openPopup();
 
 //Define an array of Latlng objects (points along the line)
 var polylinePoints = [
-            new L.LatLng(51.51032167, -0.187084072),
-            new L.LatLng(51.51019814, -0.187030437),
-            new L.LatLng(51.51013137, -0.187845822),
-            new L.LatLng(51.50457546, -0.185415744),
-            new L.LatLng(51.50476244, -0.181875224),
-            new L.LatLng(51.50457546, -0.179622177),
-            new L.LatLng(51.50409462, -0.175459380),
-            new L.LatLng(51.50368057, -0.174365042),
-            new L.LatLng(51.50299938, -0.174729820),
-            new L.LatLng(51.50213117, -0.174686903),
-            new L.LatLng(51.50199760, -0.177412030),
-            new L.LatLng(51.50179725, -0.180373197),
-            new L.LatLng(51.50143660, -0.180351735),
+            new L.LatLng(9.811916, 80.238647),
+            new L.LatLng(-21.125498, 53.789063),
+            new L.LatLng(-41.244772, 20.390625),
+            new L.LatLng(14.944785, -21.445313),
+            new L.LatLng(36.809285, -8.525391)
          ];
 
 var polylineOptions = {
     color: 'blue',
-    weight: 6,
+    weight: 5,
     opacity: 0.9
 };
 
-var polyline = new L.Polyline(polylinePoints, polylineOptions);
+var addMarker = function(lat, lng, msg){
+    L.marker([lat, lng]).addTo(map)
+        .bindPopup(msg)
+        .openPopup();
+};
 
-map.addLayer(polyline);
+var addNewPolyline = function(polylinePoints, polylineOptions){
+    var polyline = new L.Polyline(polylinePoints, polylineOptions);
+    map.addLayer(polyline);
+    // zoom the map to the polyline
+    map.fitBounds(polyline.getBounds());
+};
 
-// zoom the map to the polyline
-map.fitBounds(polyline.getBounds());
+addMarker(9.811916, 80.238647, 'Starting point');
+addMarker(36.809285, -8.525391, 'End point');
+addLayerControl(defaultLayerString);
+addNewPolyline(polylinePoints, polylineOptions);
+
